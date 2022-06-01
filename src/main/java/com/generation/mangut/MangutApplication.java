232466e679b1733b66268fa6db3c07ae4c4ae15d
João Vitor Fernandes <joaovitorcarvalhofernandes@hotmail.com>
2022-06-01 10:09:40 -0300

    Projeto Spring

---
+++ src/main/java/com/generation/mangut/MangutApplication.java
@@ -0,0 +1,13 @@
+package com.generation.mangut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/mangut/MangutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mangut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
