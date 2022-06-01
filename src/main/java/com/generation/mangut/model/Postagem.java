ef372a6ef0816cdcc88730ed7094d1b3769e3080
Vinicius Bersano <tk3.bersano@hotmail.com>
2022-06-01 11:21:26 -0300

    criacaoClasseModelTemaPostagem

---
+++ src/main/java/com/generation/mangut/model/Postagem.java
@@ -0,0 +1,55 @@
+package com.generation.mangut.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@UpdateTimestamp
+	private LocalDateTime date;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+}
---
+++ src/main/java/com/generation/mangut/model/Tema.java
@@ -0,0 +1,71 @@
+package com.generation.mangut.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size (min = 5, max = 100, message = "A descrição deve ter no mínimo 5 caracteres, e no máximo 100")
+	private String descricao;
+	
+	@NotBlank
+	@Size (min = 5, max = 20, message = "A categoria deve ter no mínimo 5 caracteres, e no máximo 100")
+	private String categoria;
+	
+	@OneToMany (mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ("tema")
+	private List<Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+
+}
