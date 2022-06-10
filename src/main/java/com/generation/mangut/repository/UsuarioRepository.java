4dee10a68c71ba7406450e7849d4d077fc39c272
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-06-10 09:47:42 -0300

    Add security e service

---
+++ src/main/java/com/generation/mangut/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.generation.mangut.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.mangut.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByUsuarioContainingIgnoreCase(String usuario);
+
+}
