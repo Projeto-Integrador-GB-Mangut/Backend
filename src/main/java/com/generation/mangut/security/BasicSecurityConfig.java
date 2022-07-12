fe14168fcbe53dc9ec3dbb847799db7635833ed9
Vinicius Bersano <tk3.bersano@hotmail.com>
2022-07-12 13:23:20 -0300

    BasicSecurityConfig mod PUT usuario

--- src/main/java/com/generation/mangut/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/mangut/security/BasicSecurityConfig.java
@@ -41,6 +41,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		 http.authorizeRequests()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
+			.antMatchers(HttpMethod.PUT, "/usuarios/atualizar").permitAll()
 			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
